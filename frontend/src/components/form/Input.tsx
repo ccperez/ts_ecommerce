daaf4a789aa366d39f3fc8cb2b509902429d488e
Conrad Perez <conradperez@Conrads-MacBook-Pro.local>
2023-06-09 18:17:24 +0800

    20_FE_PasswordShowHide

--- frontend/src/components/form/Input.tsx
+++ frontend/src/components/form/Input.tsx
@@ -1,21 +1,55 @@
-import { Form } from 'react-bootstrap'
+import { useState } from 'react'
+import { Button, Form, InputGroup } from 'react-bootstrap'
 
 interface InputProps {
-	type: string,
-	name: string,
-	label: string,
-	onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  type: string,
+  name: string,
+  label: string,
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 
 export default function Input({ type, name, label, onChange }: InputProps) {
-	return (
-		<Form.Group className="mb-3" controlId={name}>
-			<Form.Label>{label}</Form.Label>
-			<Form.Control
-				required
-				type={type}
-				onChange={onChange}
-			/>
-		</Form.Group>
-	)
+  const [passwordType, setPasswordType] = useState('password')
+
+  const togglePassword = () =>
+    setPasswordType(passwordType === 'password' ? 'text' : 'password')
+
+  const formInput = (type: string) => {
+    switch (type) {
+      default:
+        return (
+          <Form.Control
+            required
+            type={type}
+            onChange={onChange}
+          />
+        )
+    }
+  }
+
+  return (
+    <Form.Group className="mb-3" controlId={name}>
+      <Form.Label>{label}</Form.Label>
+      {name === 'password' ? (
+        <InputGroup>
+          <Form.Control
+            required
+            type={passwordType}
+            onChange={onChange}
+          />
+          <Button onClick={togglePassword}>
+            <i
+              className={
+                passwordType === 'password'
+                  ? 'fa fa-eye-slash'
+                  : 'fa fa-eye'
+              }
+            />
+          </Button>
+        </InputGroup>
+      ) : (
+        formInput(type)
+      )}
+    </Form.Group>
+  )
 }
