54bfd4fdade2a8b2b399505fa23fc8e2664103d8
Conrad Perez <conradperez@Conrads-MacBook-Pro.local>
2023-06-30 08:52:45 +0800

    36_createProductionVersion

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM node:latest
+RUN yarn global add npm@9.7.2
--- backend/src/types/environment.d.ts
+++ backend/src/types/environment.d.ts
@@ -3,12 +3,13 @@ export {}
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      PORT: number
-      MONGODB_URI: string
+      PORT: string | undefined
+      DEV_MONGODB_URI: string
+      PRD_MONGODB_URI: string
       CLIENT_URL: string
       JWT_SECRET: string
       EMAIL_HOST: string
-      EMAIL_PORT: number
+      EMAIL_PORT: string | undefined
       EMAIL_USER: string
       EMAIL_PASSWORD: string
       NODE_ENV: 'test' | 'dev' | 'prod'
